acbc6a67badf516cc82d09c96283b61fc6e7f51a
Matteo Nardi <commits@matteonardi.org>
2013-10-25 11:29:31 +0200

    Add Markdown support

--- wiki/templatetags/markdown.py
+++ wiki/templatetags/markdown.py
@@ -3,7 +3,9 @@ from django import template
 from django.template.defaultfilters import stringfilter
 from django.utils.encoding import force_unicode
 from django.utils.safestring import mark_safe
-from django.utils.html import escape
+
+# If content is not trusted, we should escape it
+#from django.utils.html import escape
 
 
 register = template.Library()
@@ -11,6 +13,6 @@ register = template.Library()
 @register.filter(is_safe=True)
 @stringfilter
 def convertTxt(value):
-    return mark_safe(markdown2.markdown(escape(force_unicode(value))))
+    return mark_safe(markdown2.markdown(force_unicode(value)))
 
 register.filter('convertTxt', convertTxt)
