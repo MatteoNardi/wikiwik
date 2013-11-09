723336363841d657c815910a506f4a60531c9b9e
Matteo Nardi <commits@matteonardi.org>
2013-11-09 11:15:43 +0100

    Fix errors in previous commit

--- wiki/models.py
+++ wiki/models.py
@@ -3,7 +3,7 @@ from django.db import models
 class Page(models.Model):
     title = models.CharField(max_length=200)
     pub_date = models.DateTimeField('Publication date', auto_now_add=True)
-    mod_date = models.DateTimeField('Modification date', auto_now_add=True)
+    mod_date = models.DateTimeField('Modification date')
     slug = models.SlugField()
     author = models.ForeignKey('auth.User', related_name='pages')
     content = models.TextField(blank=True, default='')
